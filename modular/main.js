9b1f495378a81de3d27435ac93fa6e6459db1f38
aamir-yaseen-baba <aamir.yaseen@axelerant.com>
2023-06-23 17:49:40 +0530

    add modular tests

---
+++ modular/main.js
@@ -0,0 +1,19 @@
+
+import { coinflip } from "./coinflip.js";
+import { contacts } from "./contacts.js";
+import { thresholdsSettings, breakingWorkload, smokeWorkload } from "./config.js";
+
+export const options = {
+  scenarios: {
+    my_scenario: __ENV.WORKLOAD === 'breaking' ? breakingWorkload : smokeWorkload
+  },
+  thresholds: thresholdsSettings
+};
+
+const baseUrl = "https://test.k6.io";
+
+export default function () {
+  contacts(baseUrl);
+  coinflip(baseUrl);
+}
+
